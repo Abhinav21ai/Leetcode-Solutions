31832fc250b7c1233bb7a7f9cc41e029035c1461
Abhinav Sharma <abhinav21ai@gmail.com>
2025-06-02 09:19:14 +0530

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/2/2025, 9:19:16 AM
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+
+        int n = haystack.size();
+        int m = needle.size();
+
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substr(i, m) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};

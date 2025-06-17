f12e1f58730c18d0f2a9427a3a1f747adec5d978
Abhinav Sharma <abhinav21ai@gmail.com>
2025-06-17 09:39:41 +0530

    File updated at 6/17/2025, 9:39:41 AM

--- 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.cpp
+++ 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.cpp
@@ -1,18 +1,37 @@
-// Last updated: 6/17/2025, 8:23:40 AM
+// Last updated: 6/17/2025, 9:39:41 AM
 class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
-        int var = -1;
-        for(int i = 0; i<nums.size();i++){
-            int a; 
-            for(int j=i+1;j<nums.size();j++)
-            if(nums[j]>nums[i]){
-                a = nums[j] - nums[i];
-                if(a>var){
-                    var=a;
-                }
-            }
+    //     int var = -1;
+    //     for(int i = 0; i<nums.size();i++){
+    //         int a; 
+    //         for(int j=i+1;j<nums.size();j++)
+    //         if(nums[j]>nums[i]){
+    //             a = nums[j] - nums[i];
+    //             if(a>var){
+    //                 var=a;
+    //             }
+    //         }
+    //     }
+    //     return var;
+    // }
+
+    int left = 0;
+    int right = left + 1;
+    int difference = -1;
+    int maxDiff = INT_MIN;
+    while(left < nums.size() && right < nums.size()){
+        if(nums[right] > nums[left]){
+            difference = nums[right] - nums[left];
+            right++;
+        }else{
+            left++;
+            right = left + 1;
         }
-        return var;
+
+        maxDiff = max(maxDiff, difference);
+    }
+
+    return maxDiff;
     }
 };
